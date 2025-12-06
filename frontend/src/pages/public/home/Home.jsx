c847438d693e57a1079ebe4587434e8334a3e60e
Qassem-Darwish <qasm83425@gmail.com>
2025-12-06 12:34:29 +0300

    Frontend (Add login & register & forgetpassword pages)

---
+++ frontend/src/pages/public/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../../components/common/Layout';
+
+function Home() {
+    return (
+        <Layout>
+
+        </Layout>
+    );
+};
+
+export default Home;
