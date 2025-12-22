861f9d9d56fffde5c158cfc5d283e607d96f960d
Qassem-Darwish <qasm83425@gmail.com>
2025-12-22 15:04:27 +0300

    Add hero equipment section to Equipment Page

--- frontend/src/pages/public/equipment/Equipment.jsx
+++ frontend/src/pages/public/equipment/Equipment.jsx
@@ -7,6 +7,29 @@ import './equipment.css';
 function Equipment() {
     return (
         <Layout>
+
+            {/* =============== HERO =============== */}
+            <section className="hero__equipment section">
+                <div className="hero__equipment__container container grid">
+                    <h1 className="hero__equipment-title">
+                        Empower Your Dealership. Connect with Farmers.
+                    </h1>
+
+                    <p className="hero__equipment-description">
+                        Join our platform to increase equipment rentals, streamline maintenance, and grow your business.
+                    </p>
+
+                    <div className="hero__equipment-buttons">
+                        <button type="button" className='hero__equipment-singin-btn'>
+                            Log In
+                        </button>
+
+                        <button type="button" className='hero__equipment-singup-btn'>
+                            Sing Up
+                        </button>
+                    </div>
+                </div>
+            </section>
             
         </Layout>
     );
