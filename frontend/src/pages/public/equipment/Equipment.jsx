dff1142cbb02118b3bd1a69416e620d59e4388de
Qassem-Darwish <qasm83425@gmail.com>
2025-12-22 15:06:12 +0300

    Add features section to Equipment Page

--- frontend/src/pages/public/equipment/Equipment.jsx
+++ frontend/src/pages/public/equipment/Equipment.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 import Layout from '../../../components/common/Layout';
+import SectionTitle from '../../../components/title/SectionTitle';
+
+import { equipment_features_data1, equipment_features_data2 } from '../../../Data';
+
+import CropsImage from '../../../assets/corps.jpg';
+import ForestImage from '../../../assets/forest.jpg';
 
 import './equipment.css';
 
@@ -28,6 +34,97 @@ function Equipment() {
                             Sing Up
                         </button>
                     </div>
+                </div>
+            </section>
+
+            
+            {/* =============== FEATURES =============== */}
+            <section className="features__equipment">
+                <SectionTitle
+                    subtitle='compelete soluation'
+                    title='powerful features for dealers'
+                    description={
+                        'Everything you need to manage your agricultural equipment business efficiently and profitably.'
+                    }
+                />
+
+                <div className="features__equipment__container flex">
+                    <div className="features__equipment__wrapper">
+                        <div className="features__equipment-image-wrapper">
+                            <img 
+                                src={CropsImage}
+                                alt="Crops Image"
+                                className='features__equipment-img' 
+                            />
+                        </div>
+
+                        <div className="features__equipment-content">
+                            {
+                                equipment_features_data1.map( ( { icon, title, description }, index) => {
+                                    return (
+                                        <div key={index} className="features__equipment-info">
+                                            <div key={index} className="features__equipment-icon-wrapper">
+                                                <img 
+                                                    src={icon}
+                                                    alt={`Equipment-Featrues-Icon-${index + 1}`}
+                                                    className="features__equipment-icon" 
+                                                />
+                                            </div>
+
+                                            <div className="features__equipment-data">
+                                                <h6 className="features__equipment-title">
+                                                    {title}
+                                                </h6>
+
+                                                <p className="features__equipment-description">
+                                                    {description}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+
+                    <div className="features__equipment__wrapper">
+                        <div className="features__equipment-content">
+                            {
+                                equipment_features_data2.map( ( { icon, title, description }, index) => {
+                                    return (
+                                        <div key={index} className="features__equipment-info">
+                                            <div key={index} className="features__equipment-icon-wrapper">
+                                                <img 
+                                                    src={icon}
+                                                    alt={`Equipment-Featrues-Icon-${index + 1}`}
+                                                    className="features__equipment-icon" 
+                                                />
+                                            </div>
+
+                                            <div className="features__equipment-data">
+                                                <h6 className="features__equipment-title">
+                                                    {title}
+                                                </h6>
+
+                                                <p className="features__equipment-description">
+                                                    {description}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+
+                        <div className="features__equipment-image-wrapper">
+                            <img 
+                                src={ForestImage}
+                                alt="Forest Image"
+                                className='features__equipment-img' 
+                            />
+                        </div>
+                    </div>
+
                 </div>
             </section>
             
